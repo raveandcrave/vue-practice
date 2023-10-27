a725bd6eae48add7a440164b2ab8e804bf971e93
Viktor Kopan <victorkopan98@gmail.com>
2023-10-27 16:13:08 +0600

    MyDialog component

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,39 @@ import { ref } from 'vue';
 
 import PostForm from '@/components/PostForm.vue';
 import PostList from '@/components/PostList.vue';
+import MyDialog from './components/ui/MyDialog.vue';
 import type { Post } from './types';
+import MyButton from './components/ui/MyButton.vue';
 
-const posts = ref([
+const posts = ref<Post[]>([
   { id: 1, title: 'Javascript', body: 'Описание поста' },
   { id: 2, title: 'Javascript 2', body: 'Описание поста 2' },
   { id: 3, title: 'Javascript 3', body: 'Описание поста 3' }
 ]);
 
+const dialogVisible = ref(false);
+
 const createPost = (post: Post) => {
   posts.value.push(post);
+  dialogVisible.value = false;
 };
 
 const removePost = (post: Post) => {
   posts.value = posts.value.filter((item) => item.id !== post.id);
 };
+
+const showDialog = () => {
+  dialogVisible.value = true;
+};
 </script>
 
 <template>
   <div class="app">
-    <PostForm @create="createPost" />
+    <h1>Страница с постами</h1>
+    <MyButton @click="showDialog" style="margin: 15px 0">Создать пост</MyButton>
+    <MyDialog v-model:show="dialogVisible">
+      <PostForm @create="createPost" />
+    </MyDialog>
     <PostList @remove="removePost" :posts="posts" />
   </div>
 </template>
---
+++ src/components/ui/MyDialog.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+interface Props {
+  show: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  show: true
+});
+
+const emit = defineEmits<{
+  (e: 'update:show', show: boolean): void;
+}>();
+
+const hideDialog = () => {
+  emit('update:show', false);
+};
+</script>
+
+<template>
+  <div class="dialog" v-if="show" @click="hideDialog">
+    <div @click.stop class="dialog__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.dialog {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  display: flex;
+}
+
+.dialog__content {
+  margin: auto;
+  background: white;
+  border-radius: 12px;
+  min-height: 50px;
+  min-width: 300px;
+  padding: 20px;
+}
+</style>
